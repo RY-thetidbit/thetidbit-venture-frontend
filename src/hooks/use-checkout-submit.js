83fce5acab8d029bd5bd206563c72e2b0f4c1e6b
tidbit <134474587+RY-thetidbit@users.noreply.github.com>
2025-03-02 12:55:40 +0530

    hardcoaded amount removed

--- src/hooks/use-checkout-submit.js
+++ src/hooks/use-checkout-submit.js
@@ -309,7 +309,7 @@ const useCheckoutSubmit = () => {
 
   const handlePaymentWithPhonePay = async (order) => {
     try {
-      order.totalAmount=1;
+      // order.totalAmount=1;
       const resData = await createPaymentIntentPhonePay(order);
       console.log("paymentIntentData:",resData);
       console.log("response:",resData.response.redirectUrl);
